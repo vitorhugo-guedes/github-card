57c8476da1b805debefb0c1e8a7253202c966206
vitorhugo-guedes <vitoroliveira.guedes@outlook.com>
2021-12-07 18:14:32 -0300

    main

--- src/script.js
+++ src/script.js
@@ -3,25 +3,39 @@ const openDescBtn = document.querySelector('#openDesc');
 const openDescIcon = document.querySelector('#openDescIcon');
 const desc = document.querySelector('#desc');
 
+const background = document.querySelector('main');
+const socialLinks = document.querySelectorAll('#link');
+const darkModeBtn = document.querySelector('#toggle-dark');
+const darkModeIcon = document.querySelector('#toggle-dark-icon');
+
+// show aside button
 cardWrapper.addEventListener('mouseover', ()=>{
     if(openDescBtn.classList.contains('open-desc-active')){
         return
     }else{
         openDescBtn.classList.toggle('open-desc-active');
-        openDescIcon.classList.remove('hide');
+        setInterval(openDescIcon.classList.remove('hide'), 5000);
     }
 })
-
 cardWrapper.addEventListener('mouseout', ()=>{
     if(openDescBtn.classList.contains('open-desc-active')){
         openDescBtn.classList.toggle('open-desc-active');
         openDescIcon.classList.add('hide');
     }
 })
-
 openDescBtn.addEventListener('click', ()=>{
     desc.classList.toggle('show');
     if(desc.classList.contains('show')){
         openDescBtn.classList.add('hide');
     }
+})
+
+// dark mode
+darkModeBtn.addEventListener('click', () => {
+    cardWrapper.classList.toggle('dark-mode');
+    darkModeIcon.classList.toggle('dark-mode');
+    socialLinks.forEach(link => {
+        link.classList.toggle('dark-mode');
+    })
+    background.classList.toggle('dark-mode-main');
 })
