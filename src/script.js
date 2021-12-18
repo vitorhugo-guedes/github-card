0f5e0db088541211333a0bc1fd382f8e1c94dc32
vitorhugo-guedes <vitoroliveira.guedes@outlook.com>
2021-12-18 13:10:54 -0300

    Responsive improvements, script improvements

--- src/script.js
+++ src/script.js
@@ -7,55 +7,62 @@ const openDescIcon = document.querySelector('#openDescIcon');
 const desc = document.querySelector('#desc');
 const descBtns = document.querySelector('.desc-btns');
 
-const background = document.querySelector('main');
+const background = document.querySelector('body');
 const darkModeBtn = document.querySelector('#toggle-dark-icon');
 
-// show and hide aside button
+const addClass = (cssClass, ...elements) => {
+    const elementList = [...elements]
+    elementList.forEach(element => {
+        element.classList.add(`${cssClass}`);
+    })
+}
+const removeClass = (cssClass, ...elements) => {
+    const elementList = [...elements]
+    elementList.forEach(element => {
+        element.classList.remove(`${cssClass}`);
+    })
+}
+
+// show aside button
 cardWrapper.addEventListener('mouseover', ()=>{
-    openDescBtn.classList.add('open-desc-active');
-    openDescIcon.classList.remove('hide');
-    card.classList.remove('border-right');
+    addClass('open-desc-active', openDescBtn);
+    removeClass('hide', openDescIcon);
 })
+// hide aside button
 cardWrapper.addEventListener('mouseout', ()=>{
-    openDescBtn.classList.remove('open-desc-active');
-    openDescIcon.classList.add('hide');
-    card.classList.add('border-right');
+    addClass('hide', openDescIcon);
+    removeClass('open-desc-active', openDescBtn);
 })
+
 // open description
 openDescBtn.addEventListener('click', ()=>{
-    openDescBtn.classList.add('hide');
-    desc.classList.remove('hide');
-    descBtns.classList.remove('hide');
+    addClass('hide', openDescBtn);
+    removeClass('hide', desc, descBtns);
 })
 // close description
 const closeDescBtn = document.querySelector('#close-desc');
 
 closeDescBtn.addEventListener('click', ()=>{
-    desc.classList.add('hide');
-    descBtns.classList.add('hide');
-    openDescBtn.classList.remove('open-desc-active');
-    openDescBtn.classList.remove('hide');
+    let hideElements = [desc, descBtns, openDescIcon]
+    addClass('hide', ...hideElements);
+
+    removeClass('hide', openDescBtn);
+    removeClass('open-desc-active', openDescBtn);
 })
 
 // dark mode
 let darkMode = localStorage.getItem('darkMode');
 
 const addDarkMode = () =>{
-    cardWrapper.classList.add('dark-mode');
-    background.classList.add('dark-mode');
-    
+    addClass('dark-mode', cardWrapper, background);
     darkMode = localStorage.setItem('darkMode', 'enabled');
 }
 const removeDarkMode = ()=>{
-    cardWrapper.classList.remove('dark-mode');
-    background.classList.remove('dark-mode');
-    
+    removeClass('dark-mode',  cardWrapper, background);
     darkMode = localStorage.setItem('darkMode', null);
 }
-
 if(darkMode == 'enabled')
     addDarkMode();
-
 darkModeBtn.addEventListener('click', () => {
     darkMode = localStorage.getItem('darkMode');
 
